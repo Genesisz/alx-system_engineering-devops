a3f63298134ddd9eca1ce657379da289806bae2e
root <genesiscostantine@gmail.com>
2023-05-18 12:00:19 +0000

    xla-desserts

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -5,9 +5,10 @@ exec { 'increase-hard-file-limit-for-holberton-user':
   command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
-
+e
 # Increase soft file limit for user holberton
 exec { 'increase-soft-file-limit-for-holberton-user':
   command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
+
