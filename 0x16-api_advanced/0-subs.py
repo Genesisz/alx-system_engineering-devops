3a5448062704fe2d8f335dc537a03d696fb151f6
Genesisz <genesiscostantine@gmail.com>
2023-08-09 18:58:57 -0700

    secondtermcharm

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """
-Number of subscribers for a given subreddit
+number of subscribers for a given subreddit
 """
 
 from requests import get
 
+
 def number_of_subscribers(subreddit):
     """
-    Function that queries the Reddit API and returns the number of subscribers
+    function that queries the Reddit API and returns the number of subscribers
     (not active users, total subscribers) for a given subreddit.
     """
 
@@ -15,11 +16,13 @@ def number_of_subscribers(subreddit):
         return 0
 
     user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = get(url, headers=user_agent)
+    results = response.json()
+
     try:
-        response = get(url, headers=user_agent)
-        results = response.json()
         return results.get('data').get('subscribers')
+
     except Exception:
         return 0
+
--- 0x16-api_advanced/README.md
+++ 0x16-api_advanced/README.md
@@ -1,2 +1,12 @@
-0x16-api_advanced
+0x16-api_advance
+
+Function Description
+The script contains a function top_ten(subreddit) that queries the Reddit API and prints the titles of the first 10 hot posts listed for the given subreddit.
+
+Parameters
+subreddit: The name of the subreddit for which you want to fetch the top posts.
+Notes
+Ensure you have a stable internet connection while running the script.
+The provided User-Agent header is set to your PC's address. Adjust it if needed.
+Invalid subreddits or issues with the Reddit API may result in non-optimal behavior
 
